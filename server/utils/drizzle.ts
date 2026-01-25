37c7f192316c807d82039903228407bb8e2e8ff3
Kmilo <kmilogp8496@gmail.com>
2026-01-25 16:10:02 +0100

    Use bun native postgresql driver

--- server/utils/drizzle.ts
+++ server/utils/drizzle.ts
@@ -1,10 +1,9 @@
-import { drizzle } from 'drizzle-orm/postgres-js'
+import { drizzle } from 'drizzle-orm/bun-sql'
 
 import * as schema from '../database/schema'
 
 export function useDrizzle() {
   return drizzle({
-    // @ts-expect-error - Connection bullshit
     connection: {
       host: process.env.DATABASE_HOST!,
       port: Number(process.env.DATABASE_PORT!),
